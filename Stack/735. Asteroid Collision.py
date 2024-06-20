c5b1831b6d9493bda46d07aa5472bda6c3f94029
youssefkhalil320 <youssefkhalil320@gmail.com>
2024-06-20 22:24:47 +0300

    735. Asteroid Collision

---
+++ Stack/735. Asteroid Collision.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        stack = []
+
+        for i in asteroids:
+            # We need to handle collision scenarios
+            while stack and i < 0 < stack[-1]:
+                if stack[-1] < -i:
+                    stack.pop()
+                    continue
+                elif stack[-1] == -i:
+                    stack.pop()
+                break
+            else:
+                stack.append(i)
+
+        return stack
