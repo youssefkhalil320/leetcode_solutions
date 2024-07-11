e8a9ce32c2779d49d419fc47097a290a759c2e04
youssefkhalil320 <youssefkhalil320@gmail.com>
2024-07-12 01:47:42 +0300

    1068. Product Sales Analysis I

---
+++ SQL/1068. Product Sales Analysis I.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+SELECT 
+    p.product_name, 
+    s.year, 
+    s.price
+FROM 
+    sales s
+LEFT JOIN 
+    product p
+ON
+    s.product_id = p.product_id
